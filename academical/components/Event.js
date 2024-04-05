960113d6fccd9c61e10c50d827e6fa92d0ea5ab7
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-05 18:05:56 -0400

    fixed date

--- academical/components/Event.js
+++ academical/components/Event.js
@@ -1,18 +1,13 @@
 import React from "react";
+import EventDate from "./EventDate";
+
 const event = (event) => {
-
-    const EventDate = (event) => {new Date(event.date).toLocaleDateString('en-US', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
-    })};
-
     return (
         <li key={event.id} className="event-list-item">
             <img src={event.image} alt={event.title} className="event-list-image" />
             <div className="event-list-details">
                 <h2 className="event-list-title">{event.title}</h2>
-                <EventDate date={event.date} />
+                <EventDate date={event.date}/>
                 <p className="event-list-description">{event.description}</p>
             </div>
         </li>
