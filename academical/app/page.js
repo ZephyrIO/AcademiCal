e36758ac821af6862d48c97eb1820c9c4d8269be
Shad0wVort3x <connectkedar09@gmail.com>
2024-04-05 17:00:00 -0400

    Update page.js

--- academical/app/page.js
+++ academical/app/page.js
@@ -1,1 +1,26 @@
+import AddEvent from "@/components/AddEvent";
+import {useState} from 'react';
 
+
+function Home() {
+    const testEvent = [
+        {
+            id: '1',
+            title: 'test',
+            description: 'test',
+            image: 'https://randomuser.me/api/portraits/men/75.jpg'
+        }
+    ];
+    const  [events, setEvents] = useState(testEvent);
+    const addEventHandler = (event) => {
+        setEvents([...events, event]);
+    };
+        
+    return (
+        <div>  
+            <AddEvent onAdd={addEventHandler}/>
+        </div>
+    );
+    
+}
+export default Home;
