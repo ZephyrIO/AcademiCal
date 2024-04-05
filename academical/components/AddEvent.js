0ccf630655eb89fe901bf826a69eb22888a3bb1d
Shad0wVort3x <connectkedar09@gmail.com>
2024-04-05 16:23:09 -0400

    Create AddEvent.js

---
+++ academical/components/AddEvent.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+import Card from './Card';
+import Button from './Button';
+
+const AddEvent = (props) => {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+
+    
+}
