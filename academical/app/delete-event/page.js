2ab98ad4b600d409f342a0575197a62c3ff66584
Shad0wVort3x <connectkedar09@gmail.com>
2024-04-19 19:28:58 -0400

    delete

--- academical/app/delete-event/page.js
+++ academical/app/delete-event/page.js
@@ -1,2 +1,16 @@
+'use client'
+import {useState} from 'react';
+import DeleteEvent from "@/components/DeleteEvent";
+import EventList from "@/components/EventList";
+import Header from '@/components/Header';
+import './page.css';
+
+
 export default function DeleteEventView() {
+    return (
+        <div>
+            <h1>Delete Event</h1>
+        </div>
+    )
+
 }
