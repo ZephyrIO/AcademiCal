d0b28b52fd00dbf6d7213103a4ca7a418999875b
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-19 21:54:39 -0400

    auto return to home page after delete

--- academical/components/DeleteEvent.js
+++ academical/components/DeleteEvent.js
@@ -2,45 +2,50 @@ import React, { useState, useEffect } from 'react';
 import Button from './Button';
 import './DeleteEvent.css';
 import axios from 'axios';
+import { useRouter } from 'next/navigation'
+
 const DeleteEvent = () => {
-    const [currentEvents, setEvents] = useState([]);
-  
-    useEffect(() => {
-      axios.get('http://localhost:8085/calendar/events')
-        .then(response => {
-          console.log('Events fetched successfully:', response.data);
-          setEvents(response.data);
-        })
-        .catch((error) => {
-          console.error('Failed to fetch events:', error);
-        });
-    }, []);
+  const router = useRouter();
+  const [currentEvents, setEvents] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8085/calendar/events')
+      .then(response => {
+        console.log('Events fetched successfully:', response.data);
+        setEvents(response.data);
+      })
+      .catch((error) => {
+        console.error('Failed to fetch events:', error);
+      });
+  }, []);
 
 
 
-    const deleteEventHandler = (id) => {
-        axios.delete(`http://localhost:8085/calendar/events/${id}`)
-          .then(response => {
-            console.log('Event deleted successfully:', response.data);
-            setEvents(prevEvents => prevEvents.filter(event => event._id !== id));
-          })
-          .catch((error) => {
-            console.error('Failed to delete event:', error);
-          });
-      };
-    
-      return (
-        <div>
-          {currentEvents && currentEvents.map(event => (
-            <div key={event._id}>
-              <h2>{event.title}</h2>
-              <div className="delete-button">
-                <Button onClick={() => deleteEventHandler(event._id)}>Delete Event</Button>
-              </div>
-            </div>
-          ))}
+  const deleteEventHandler = (id) => {
+    axios.delete(`http://localhost:8085/calendar/events/${id}`)
+      .then(response => {
+        console.log('Event deleted successfully:', response.data);
+        setEvents(prevEvents => prevEvents.filter(event => event._id !== id));
+      })
+      .catch((error) => {
+        console.error('Failed to delete event:', error);
+      });
+
+    router.push('/');
+  };
+
+  return (
+    <div>
+      {currentEvents && currentEvents.map(event => (
+        <div key={event._id}>
+          <h2>{event.title}</h2>
+          <div className="delete-button">
+            <Button onClick={() => deleteEventHandler(event._id)}>Delete Event</Button>
+          </div>
         </div>
-      );
-    };
-    
-    export default DeleteEvent;
+      ))}
+    </div>
+  );
+};
+
+export default DeleteEvent;
