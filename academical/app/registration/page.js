055e8b1685c9871930d78dad8d2aaaf47d52c413
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-19 20:15:58 -0400

    created login and register pages

--- academical/app/registration/page.js
+++ academical/app/registration/page.js
@@ -1,11 +1,14 @@
 'use client'
 import { useState } from 'react';
 import Header from '@/components/Header';
+import Link from 'next/link';
+import Button from '@/components/Button';
 
 export default function Registration() {
     return (
         <div>
             <Header />
+            <Link href={'/login'}><Button>Login</Button></Link>
         </div>
     );
 }
