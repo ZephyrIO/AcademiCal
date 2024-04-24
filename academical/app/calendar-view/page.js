e3045a035ddd7bd068cf09c57410633f3734c2b4
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-23 21:42:52 -0400

    added header to page

--- academical/app/calendar-view/page.js
+++ academical/app/calendar-view/page.js
@@ -1,10 +1,12 @@
 'use client';
 import React from 'react';
 import Calendar from '@/components/Calendar';
+import Header from '@/components/Header';
 
 const CalendarView = () => {
   return (
     <div>
+      <Header />
       <h1>Calendar</h1>
       <Calendar />
     </div>
