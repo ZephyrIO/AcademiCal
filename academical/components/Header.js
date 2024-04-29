2dcec36c0bdf9be3a552768a8e172af4d1727c52
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-28 20:14:35 -0400

    fixed spacing

--- academical/components/Header.js
+++ academical/components/Header.js
@@ -1,11 +1,11 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Button from "./Button";
 import './Header.css';
 import Link from 'next/link';
 import UserContext from '../context/UserContext';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-export default function Header({ userData, logoutHandler, isLoggedIn, handleLogin}) {
+export default function Header({ userData, logoutHandler, isLoggedIn, handleLogin }) {
     const userContext = useContext(UserContext);
     useEffect(() => {
         if (userData && userData.token) {
@@ -19,9 +19,9 @@ export default function Header({ userData, logoutHandler, isLoggedIn, handleLogi
         <div className="header">
             <Link href="/calendar-view"> <Button>Calendar</Button> </Link>
             <h1 className="title">AcademiCal</h1>
-                        {isLoggedIn ? (
-                        <Link href="/"><Button onClick={logoutHandler}>Logout</Button></Link> ) : (
-                        <Link href="/login"><Button>Login</Button></Link>)}
-                    </div>
-                );
-            }
+            {isLoggedIn ? (
+                <Link href="/"><Button onClick={logoutHandler}>Logout</Button></Link>) : (
+                <Link href="/login"><Button>Login</Button></Link>)}
+        </div>
+    );
+}
