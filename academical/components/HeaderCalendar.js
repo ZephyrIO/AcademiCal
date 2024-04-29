cba6e6404888ebf88333d577a5dd30be85d824d6
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2024-04-28 20:13:30 -0400

    fixed the calendar page header

--- academical/components/HeaderCalendar.js
+++ academical/components/HeaderCalendar.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import Button from "./Button";
 import './Header.css';
 import Link from 'next/link';
+import UserContext from '../context/UserContext';
+import { useState, useEffect } from 'react';
 
-export default function HeaderCalendar(props) {
+export default function HeaderCalendar({ userData, logoutHandler, isLoggedIn, handleLogin }) {
+    const userContext = useContext(UserContext);
+    useEffect(() => {
+        if (userData && userData.token) {
+            isLoggedIn = true;
+        } else {
+            isLoggedIn = false;
+        }
+    }, [userData, handleLogin]);
 
     return (
         <div className="header">
             <Link href="/"> <Button>Home</Button> </Link>
             <h1 className="title">AcademiCal</h1>
-            {props.loggedIn ?
-                <Link href="/"><Button onClick={props.loginHandler}>Logout</Button></Link> :
-                <Button onClick={props.loginHandler}>Login</Button>}
+            {isLoggedIn ? (
+                <Link href="/"><Button onClick={logoutHandler}>Logout</Button></Link>) : (
+                <Link href="/login"><Button>Login</Button></Link>)}
         </div>
     );
 }
